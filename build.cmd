f41ebaebd028efef1cb1e2bdd7ade1d76b682d6b
le674 <adamakkouche42@yahoo.fr>
2024-12-15 10:11:19 +0000

    remove file and add build pack

--- build.cmd
+++
@@ -1,5 +0,0 @@
-# Construire l'image Docker
-docker build -t c-docker-app .
-
-# Exécuter l'image Docker
-docker run --rm c-docker-app
--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 # Construire l'image Docker
 docker build -t c-docker-app .
-
 # Exécuter l'image Docker
 docker run --rm c-docker-app
+#Build a package
+gcc -o app main.c
